99f2a7a3090f00a335558c97e14f60aabddb1479
Earl Platt III <36335481+eaplatt3@users.noreply.github.com>
2020-01-27 13:52:36 -0500

    Add project files.

---
+++ BoilerPlateBST/BinarySearchTree.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoilerPlateBST
+{
+    class BinarySearchTree
+    {
+        public class Node
+        {
+            public int data;
+            public Node left;
+            public Node right;
+
+            public void DisplayNode()
+            {
+                Console.Write(data + " ");
+            }
+        }
+
+        public Node root;
+
+        public BinarySearchTree()
+        {
+            root = null;
+        }
+    }
+}
